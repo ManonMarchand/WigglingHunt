beee375d9fe7f454b037f35bf7b460c63b8a0783
Xwilarg <xwilarg@protonmail.com>
2023-03-10 23:42:21 +0100

    Add spawn management

---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,38 @@
+﻿using ScientificGameJam.SO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
+
+namespace ScientificGameJam.Player
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform[] _spawnPoint;
+
+        private PlayerSpawn[] _spawns;
+
+        private void Awake()
+        {
+            _spawns = _spawnPoint.Select(x => new PlayerSpawn(x)).ToArray();
+        }
+
+        public void OnPlayerJoin(PlayerInput player)
+        {
+            var freeSpot = Array.IndexOf(_spawns, _spawns.FirstOrDefault(x => x.Player == null));
+            Assert.AreNotEqual(-1, freeSpot);
+            _spawns[freeSpot].Player = player;
+            player.transform.position = _spawns[freeSpot].Spawn.position;
+        }
+
+        public void OnPlayerLeave(PlayerInput player)
+        {
+            var freeSpot = Array.IndexOf(_spawns, _spawns.Select(x => x.DoesContainsPlayer(player)));
+            Assert.AreNotEqual(-1, freeSpot);
+            _spawns[freeSpot].Player = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerSpawn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace ScientificGameJam.Player
+{
+    public class PlayerSpawn
+    {
+        public PlayerSpawn(Transform spawn)
+        {
+            Spawn = spawn;
+        }
+
+        public bool DoesContainsPlayer(PlayerInput p)
+            => Player.GetInstanceID() == p.GetInstanceID();
+
+        public PlayerInput Player { set; get; }
+        public Transform Spawn { private set; get; }
+    }
+}
