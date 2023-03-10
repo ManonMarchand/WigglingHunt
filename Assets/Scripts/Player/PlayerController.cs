5d5bb13033abe3857ceec47a02172c8e1ca16dbb
Xwilarg <xwilarg@protonmail.com>
2023-03-10 23:27:27 +0100

    Add player controller

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,31 @@
+using ScientificGameJam.SO;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace ScientificGameJam.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        private PlayerInfo _info;
+
+        private Rigidbody2D _rb;
+
+        private Vector2 _mov;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.velocity = _mov * _info.Speed * Time.fixedDeltaTime;
+        }
+
+        public void Move(InputAction.CallbackContext value)
+        {
+            _mov = value.ReadValue<Vector2>().normalized;
+        }
+    }
+}
