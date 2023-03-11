fbe59460e6e5f3604c55c3c212ab5d4e27963ce7
Xwilarg <xwilarg@protonmail.com>
2023-03-11 17:47:05 +0100

    Add random sprite

---
+++ Assets/Scripts/Player/SelectRandomSprite.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class SelectRandomSprite : MonoBehaviour
+    {
+        [SerializeField]
+        private Sprite[] _sprites;
+
+        private void Awake()
+        {
+            GetComponent<SpriteRenderer>().sprite = _sprites[Random.Range(0, _sprites.Length)];
+        }
+    }
+}
