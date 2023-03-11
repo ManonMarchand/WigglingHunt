2bb9b13a033cff056502f829ef6ab4e34b489e81
Xwilarg <xwilarg@protonmail.com>
2023-03-11 21:00:55 +0100

    Add first menu

---
+++ Assets/Scripts/Menu/PlayMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ScientificGameJam.Menu
+{
+    public class PlayMenu : MonoBehaviour
+    {
+        public void Play()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
