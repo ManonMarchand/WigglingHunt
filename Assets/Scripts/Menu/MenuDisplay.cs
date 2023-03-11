6402c17fc20b1ac38dcc5a29083dab5f882cdc4f
Xwilarg <xwilarg@protonmail.com>
2023-03-11 17:03:40 +0100

    Add menu info panel

---
+++ Assets/Scripts/Menu/MenuDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace ScientificGameJam.Menu
+{
+    public class MenuDisplay : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _credits;
+
+        public void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("InfoPanel"))
+            {
+                Toggle(collision.GetComponent<InfoPanel>().Tag, true);
+            }
+        }
+
+        public void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.CompareTag("InfoPanel"))
+            {
+                Toggle(collision.GetComponent<InfoPanel>().Tag, false);
+            }
+        }
+
+        private void Toggle(string tag, bool value)
+        {
+            switch (tag)
+            {
+                case "Credits": _credits.SetActive(value); break;
+                default: throw new NotImplementedException();
+            }
+        }
+    }
+}
