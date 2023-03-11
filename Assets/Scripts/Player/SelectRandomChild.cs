a0e67b23f6f687142c3f122092a257e7195f86da
Xwilarg <xwilarg@protonmail.com>
2023-03-11 17:56:03 +0100

    Add sprites

---
+++ Assets/Scripts/Player/SelectRandomChild.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class SelectRandomChild : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] _sprites;
+
+        private void Awake()
+        {
+            _sprites[Random.Range(0, _sprites.Length)].SetActive(true);
+        }
+    }
+}
